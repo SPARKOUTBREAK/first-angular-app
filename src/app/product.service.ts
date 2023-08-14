80a9540d56dc0dec18c76f8386a98c07a0063146
spark <vamsi0503@gmail.com>
2023-08-14 14:55:36 -0400

    Debugged product variable and component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getAllProducts(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:8080/api/products'); // adjust the URL based on your Spring Boot app's endpoint
+  }
+  
+  
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  constructor(private productService: ProductService) { }
+  products: any[] = [];
+
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+  
+
 
 }
